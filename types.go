fda485ecbdcda8ed8ca42483f215a83a9af8824a
Dmitry Morozov <dmorozov@valoru-software.com>
2024-02-29 20:35:04 +0600

    rename basic package

--- types.go
+++ types.go
@@ -4,7 +4,7 @@
  * Copyright (c) 2023.
  */
 
-package basic_utils
+package basicutils
 
 type Numeric interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
