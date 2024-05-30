82c56ee6b0196e022fc555f98771be3e37a0c51c
Dmitry Morozov <dmorozov@valoru-software.com>
2024-05-30 19:12:06 +0500

    ucache: cleanup ManagedCache code

--- ucache/managed_test.go
+++ ucache/managed_test.go
@@ -1,51 +1,169 @@
-//go:build integration_test
-
 /*
  * @kordax (Dmitry Morozov)
  * dmorozov@valoru-software.com
  * Copyright (c) 2024.
  */
 
-package ucache
+package ucache_test
 
 import (
 	"testing"
 	"time"
 
+	"github.com/kordax/basic-utils/ucache"
 	"github.com/kordax/basic-utils/uopt"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestManagedCache_TTL(t *testing.T) {
-	ttl := 500 * time.Millisecond
-	cache := NewInMemoryHashMapCache[IntKey, string, uint64](func(key int64) uint64 {
-		return uint64(key)
-	}, uopt.Of(ttl))
-	managedCache := NewManagedCache(cache, uopt.Of(ttl))
+func TestManagedMultiCache_SetAndGet(t *testing.T) {
+	cache := ucache.NewInMemoryTreeMultiCache[ucache.StrCompositeKey, DummyComparable](uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedMultiCache(cache, time.Second)
 	defer managedCache.Stop()
 
-	key := IntKey(1)
+	key := ucache.NewStrCompositeKey("category", "key1")
+	value := DummyComparable{Val: 42}
+
+	managedCache.Set(key, value)
+	results := managedCache.Get(key)
+	assert.Len(t, results, 1)
+	assert.Equal(t, value, results[0])
+}
+
+func TestManagedMultiCache_Drop(t *testing.T) {
+	cache := ucache.NewInMemoryTreeMultiCache[ucache.StrCompositeKey, DummyComparable](uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedMultiCache(cache, time.Second)
+	defer managedCache.Stop()
+
+	key := ucache.NewStrCompositeKey("category", "key1")
+	value := DummyComparable{Val: 42}
+
+	managedCache.Set(key, value)
+	managedCache.Drop()
+	results := managedCache.Get(key)
+	assert.Empty(t, results)
+}
+
+func TestManagedMultiCache_DropKey(t *testing.T) {
+	cache := ucache.NewInMemoryTreeMultiCache[ucache.StrCompositeKey, DummyComparable](uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedMultiCache(cache, time.Second)
+	defer managedCache.Stop()
+
+	key := ucache.NewStrCompositeKey("category", "key1")
+	value := DummyComparable{Val: 42}
+
+	managedCache.Set(key, value)
+	managedCache.DropKey(key)
+	results := managedCache.Get(key)
+	assert.Empty(t, results)
+}
+
+func TestManagedMultiCache_PutQuietly(t *testing.T) {
+	cache := ucache.NewInMemoryTreeMultiCache[ucache.StrCompositeKey, DummyComparable](uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedMultiCache(cache, time.Second)
+	defer managedCache.Stop()
+
+	key := ucache.NewStrCompositeKey("category", "key1")
+	value := DummyComparable{Val: 42}
+
+	managedCache.PutQuietly(key, value)
+	results := managedCache.Get(key)
+	assert.Len(t, results, 1)
+	assert.Equal(t, value, results[0])
+}
+
+func TestManagedMultiCache_Outdated(t *testing.T) {
+	ttl := 100 * time.Millisecond
+	cache := ucache.NewInMemoryTreeMultiCache[ucache.StrCompositeKey, DummyComparable](uopt.Of(ttl))
+	managedCache := ucache.NewManagedMultiCache(cache, time.Nanosecond)
+	defer managedCache.Stop()
+
+	key := ucache.NewStrCompositeKey("category", "key1")
+	value := DummyComparable{Val: 42}
+
+	managedCache.Set(key, value)
+	time.Sleep(2 * ttl)
+	assert.True(t, managedCache.Outdated(uopt.Of(key)))
+}
+
+func TestManagedCache_SetAndGet(t *testing.T) {
+	cache := ucache.NewInMemoryHashMapCache[ucache.IntKey, string, uint64](func(key int64) uint64 {
+		return uint64(key)
+	}, uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedCache(cache, time.Second)
+	defer managedCache.Stop()
+
+	key := ucache.IntKey(1)
 	value := "TestValue"
 
 	managedCache.Set(key, value)
+	v, ok := managedCache.Get(key)
+	assert.True(t, ok)
+	assert.Equal(t, value, *v)
+}
 
-	time.Sleep(2 * ttl)
-	managedCache.performCleanup()
+func TestManagedCache_Drop(t *testing.T) {
+	cache := ucache.NewInMemoryHashMapCache[ucache.IntKey, string, uint64](func(key int64) uint64 {
+		return uint64(key)
+	}, uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedCache(cache, time.Second)
+	defer managedCache.Stop()
 
+	key := ucache.IntKey(1)
+	value := "TestValue"
+
+	managedCache.Set(key, value)
+	managedCache.Drop()
 	v, ok := managedCache.Get(key)
 	assert.False(t, ok)
 	assert.Nil(t, v)
+}
+
+func TestManagedCache_DropKey(t *testing.T) {
+	cache := ucache.NewInMemoryHashMapCache[ucache.IntKey, string, uint64](func(key int64) uint64 {
+		return uint64(key)
+	}, uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedCache(cache, time.Second)
+	defer managedCache.Stop()
+
+	key := ucache.IntKey(1)
+	value := "TestValue"
 
 	managedCache.Set(key, value)
-	v, ok = managedCache.Get(key)
-	assert.True(t, ok)
-	assert.NotNil(t, v)
+	managedCache.DropKey(key)
+	v, ok := managedCache.Get(key)
+	assert.False(t, ok)
+	assert.Nil(t, v)
+}
 
-	time.Sleep(2 * ttl)
-	managedCache.performCleanup()
+func TestManagedCache_SetQuietly(t *testing.T) {
+	cache := ucache.NewInMemoryHashMapCache[ucache.IntKey, string, uint64](func(key int64) uint64 {
+		return uint64(key)
+	}, uopt.Null[time.Duration]())
+	managedCache := ucache.NewManagedCache(cache, time.Second)
+	defer managedCache.Stop()
 
-	// Key should not be removed now as it was requested before cleanupByGet and it's ttl was updated
-	v, ok = managedCache.Get(key)
+	key := ucache.IntKey(1)
+	value := "TestValue"
+
+	managedCache.SetQuietly(key, value)
+	v, ok := managedCache.Get(key)
 	assert.True(t, ok)
-	assert.NotNil(t, v)
+	assert.Equal(t, value, *v)
+}
+
+func TestManagedCache_Outdated(t *testing.T) {
+	ttl := 1 * time.Millisecond
+	cache := ucache.NewInMemoryHashMapCache[ucache.IntKey, string, uint64](func(key int64) uint64 {
+		return uint64(key)
+	}, uopt.Of(ttl))
+	managedCache := ucache.NewManagedCache(cache, time.Nanosecond)
+	defer managedCache.Stop()
+
+	key := ucache.IntKey(1)
+	value := "TestValue"
+
+	managedCache.Set(key, value)
+	time.Sleep(10 * ttl)
+	_, ok := managedCache.Get(key)
+	assert.False(t, ok)
 }
--- ustr/ustr.go
+++ ustr/ustr.go
@@ -6,6 +6,8 @@
 
 package ustr
 
+import "strings"
+
 // Def behaves as Or(val, "")), so it returns default empty string if value is not present,
 func Def(val *string) string {
 	if val == nil {
@@ -13,4 +15,8 @@ func Def(val *string) string {
 	}
 
 	return *val
+}
+
+func Concat(vals ...string) string {
+	return strings.Join(vals, "")
 }
