9ead89379f244610cc90c4896f88ac2d0893cbfd
Dmitry Morozov <dmorozov@valoru-software.com>
2024-07-24 18:26:54 +0500

    uconst: UniqueKey interface

--- uconst/types.go
+++ uconst/types.go
@@ -14,9 +14,17 @@ type Comparable interface {
 }
 
 /*
+UniqueKey specifies an abstract key with an ability to provide hash.
+*/
+type UniqueKey[T comparable] interface {
+	Key() T // Key should return a unique item key. It can be a hash or just an index.
+}
+
+/*
 Unique specifies an abstract key with an ability to provide hash.
+Comparable interface can be issued to mitigate potential collisions, e.g. in hashmaps or other implementations.
 */
 type Unique interface {
 	Comparable
-	Key() int64 // Key should return a unique item key. It can be a hash or just an index.
+	UniqueKey[int64]
 }
