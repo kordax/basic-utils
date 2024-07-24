be857d58b63542d68c0aba60cd4ea1d6775e032f
Dmitry Morozov <dmorozov@valoru-software.com>
2024-07-24 19:55:50 +0500

    uset: fix ComparableHashset values

--- uset/comparable_hashset.go
+++ uset/comparable_hashset.go
@@ -6,18 +6,21 @@
 
 package uset
 
-import "github.com/kordax/basic-utils/uconst"
+import (
+	"github.com/kordax/basic-utils/uconst"
+	"github.com/kordax/basic-utils/umap"
+)
 
 // ComparableHashSet is the same as HashSet
 type ComparableHashSet[T uconst.UniqueKey[K], K comparable] struct {
-	m map[K]dummy
+	m map[K]T
 }
 
 // NewComparableHashSet creates a new instance of HashSet with the default size
 func NewComparableHashSet[T uconst.UniqueKey[K], K comparable](values ...T) *ComparableHashSet[T, K] {
-	m := make(map[K]dummy)
+	m := make(map[K]T)
 	for _, v := range values {
-		m[v.Key()] = dummy{}
+		m[v.Key()] = v
 	}
 
 	return &ComparableHashSet[T, K]{m: m}
@@ -25,13 +28,13 @@ func NewComparableHashSet[T uconst.UniqueKey[K], K comparable](values ...T) *Com
 
 // NewComparableHashSetWithSize creates a new instance of HashSet with a specified initial size
 func NewComparableHashSetWithSize[T uconst.UniqueKey[K], K comparable](size int) *ComparableHashSet[T, K] {
-	return &ComparableHashSet[T, K]{m: make(map[K]dummy, size)}
+	return &ComparableHashSet[T, K]{m: make(map[K]T, size)}
 }
 
 // Add inserts a value into the set and returns true if the value was not already present
 func (s *ComparableHashSet[T, K]) Add(value T) bool {
 	if s.m == nil {
-		s.m = make(map[K]dummy)
+		s.m = make(map[K]T)
 	}
 
 	key := value.Key()
@@ -39,14 +42,14 @@ func (s *ComparableHashSet[T, K]) Add(value T) bool {
 		return false
 	}
 
-	s.m[key] = def
+	s.m[key] = value
 	return true
 }
 
 // Contains checks if a value is present in the set
 func (s *ComparableHashSet[T, K]) Contains(value T) bool {
 	if s.m == nil {
-		s.m = make(map[K]dummy)
+		s.m = make(map[K]T)
 	}
 
 	_, exists := s.m[value.Key()]
@@ -56,7 +59,7 @@ func (s *ComparableHashSet[T, K]) Contains(value T) bool {
 // Remove deletes a value from the set and returns true if the value was present
 func (s *ComparableHashSet[T, K]) Remove(value T) bool {
 	if s.m == nil {
-		s.m = make(map[K]dummy)
+		s.m = make(map[K]T)
 	}
 
 	key := value.Key()
@@ -74,7 +77,7 @@ func (s *ComparableHashSet[T, K]) Size() int {
 
 // Clear returns the number of elements in the set
 func (s *ComparableHashSet[T, K]) Clear() {
-	s.m = make(map[K]dummy)
+	s.m = make(map[K]T)
 }
 
 // Compare compares two values of type T based on their keys and returns an integer indicating their relative order.
@@ -91,4 +94,8 @@ func (s *ComparableHashSet[T, K]) Clear() {
 // The method assumes that T implements a Key() method that returns an integer value which can be used for comparison.
 func (s *ComparableHashSet[T, K]) Compare(lhv, rhv T) bool {
 	return lhv.Key() == rhv.Key()
+}
+
+func (s *ComparableHashSet[T, K]) Values() []T {
+	return umap.Values(s.m)
 }
