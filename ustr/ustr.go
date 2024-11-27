965fc11027f09e4e4b382a10e1507fe83d368d3f
Dmitry Morozov <morozov.d@mascot.games>
2024-11-27 15:41:11 +0500

    uconst/uarray: AsString func + Stringable constraint

--- ustr/ustr.go
+++ ustr/ustr.go
@@ -6,7 +6,9 @@
 
 package ustr
 
-import "strings"
+import (
+	"strings"
+)
 
 // Def behaves as Or(val, "")), so it returns default empty string if value is not present,
 func Def(val *string) string {
